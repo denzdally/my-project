b74138310526c3958691bf0222ebe0687591a84e
denzdally <60770144+denzdally@users.noreply.github.com>
2020-02-07 14:56:14 +0800

    Create mymodule.py

---
+++ mymodule.py
@@ -0,0 +1,12 @@
+sorteddf = df.sort_values(by = ['company','body-style','price'], ascending = [True,True,False])
+
+groupbycoy = sorteddf[['body-style','price']].groupby(sorteddf['company'])
+
+# print(list(groupbycoy))
+
+for company, grps in groupbycoy:
+    print(company)
+    print('----------')
+    for i in grps.index:
+        print(grps.value[i][0], end = ' ')
+        print(grps.value[i][1])
