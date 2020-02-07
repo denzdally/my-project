ac85eb4b701b9364cf8eec7b3289fce8c21a6a00
denzdally <60770144+denzdally@users.noreply.github.com>
2020-02-07 15:01:02 +0800

    Update mymodule.py

--- mymodule.py
+++ mymodule.py
@@ -4,7 +4,7 @@ groupbycoy = sorteddf[['body-style','price']].groupby(sorteddf['company'])
 
 # print(list(groupbycoy))
 
-for company, grps in groupbycoy:
+for company2, grps in groupbycoy:
     print(company)
     print('----------')
     for i in grps.index:
